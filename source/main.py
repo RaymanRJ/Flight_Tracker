f3cbdf9f32b7fdbdf9f38a0208282eda7dd52cec
RaymanRJ <34038866+RaymanRJ@users.noreply.github.com>
2019-04-20 18:20:33 -0400

    Update main.py

--- source/main.py
+++ source/main.py
@@ -5,10 +5,17 @@ Flight Tracker
 Author: Rayman Jamal
 Date: April 20, 2019
 
-This script is designed to run as a cronjob on Ubuntu.
+This script is designed to run as a cronjob on Ubuntu, with the
+flight number  passed as a command-line argument.
 
-The flight number must be passed as a command-line argument.
+Flights are retrieved from https://www.flightaware.com using a Chrome
+browser on Selenium. This introduces some instability however, as this
+system will probably not work if any changes are made to that website.
 
+Selenium is used to avoid the need of signing up for an API key to search
+for flight times. As most websites are created through JavaScript,
+Python's request module would not work. Selenium allows JavaScript to run and
+populate the DOM, which can then be parsed after.
 """
 
 from selenium import webdriver
