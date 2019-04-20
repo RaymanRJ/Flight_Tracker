f06a4587a7362fb15482f36f9d4261f546fe7d43
RaymanRJ <34038866+RaymanRJ@users.noreply.github.com>
2019-04-20 17:04:09 -0400

    She's working!

--- source/main.py
+++ source/main.py
@@ -1,21 +1,50 @@
-import requests
+#!/usr/bin/env python
+
+##################################################
+# Flight Tracker
+# Author: Rayman Jamal
+# Date: April 20, 2019
+##################################################
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import pyttsx3
+import sys
 
+# Variables:
+# --- Shell variable:
+flight_number = sys.argv[1] # Passed through command-line
+
+# --- Script variables:
 url = "https://flightaware.com/live/flight/"
-flight_number = "wg681"
+# flight_number = "wg681"
 status_class = "flightPageArrivalDelayStatus"
+arrival_time_class = "flightPageSummaryArrival"
+on_time = "(on time)"
 
-flight_page = requests.get(url + flight_number)
+# Init objects:
 
-print(flight_page.content)
+# --- Speech engine:
+engine = pyttsx3.init()
 
-browser = webdriver.Chrome()
+# --- Chrome:
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("headless")
+
+browser = webdriver.Chrome(options=chrome_options)
+
+# Get request:
 browser.get(url + flight_number)
 
-elem = browser.find_element_by_class_name(status_class)  # Find the html box
+# Parse results:
+status = browser.find_element_by_class_name(status_class)
+time_element = browser.find_element_by_class_name(arrival_time_class)
+time = time_element.find_element_by_tag_name("em")
 
-print(elem.text)
-print(elem)
+# Speak:
 
+engine.say("The flight is " + status.text)
+engine.say("And will arrive at " + time.text)
+engine.runAndWait()
+
+# Close browser:
 browser.close()
